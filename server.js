79288a28ac4a215d1096e68db7906f05bad7ffa9
rabbaniha <hrabbaniha@gmail.com>
2024-02-19 14:28:55 +0330

    set cors

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import express from "express"
 import mongoose from "mongoose"
+import cors from "cors"
 import "dotenv/config"
 
 import connectDB from "./configs/dbConfig.js"
 
 const app = express()
-
-
-
 const port = process.env.port || 3000
 const dbUrl = process.env.MONGO_URL
+
+app.use(express.json())
+app.use(cors())
+
+
+
 
 connectDB(dbUrl)
 mongoose.connection.once("open", () => {
